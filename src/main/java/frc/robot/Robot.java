4a85a7b160c4d2aa19622d8c7a5b843ecc397aa3
Francisco Fabregat <frank@fabregat.com.mx>
2019-01-07 23:57:32 -0600

    Start setting up Robot.java, import gyroscope library

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,63 +1,60 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
+package main.java.frc.robot;
 
-package frc.robot;
+import com.analog.adis16448.frc.ADIS16448_IMU;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the TimedRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the build.gradle file in the
- * project.
- */
 public class Robot extends TimedRobot {
-  public static ExampleSubsystem m_subsystem = new ExampleSubsystem();
-  public static OI m_oi;
 
-  Command m_autonomousCommand;
-  SendableChooser<Command> m_chooser = new SendableChooser<>();
+  /* Initialize autonomousCommand to store chosen command */
+  Command autonomousCommand;
 
-  /**
-   * This function is run when the robot is first started up and should be
-   * used for any initialization code.
-   */
+  /* Initialize OI and Subsystems */
+  public static OI oi;
+
+  /* Initialize and define NetworkTable 'SmartDashbaord' */
+  NetworkTable table = NetworkTable.getTable("SmartDashboard");
+
+  /* Define default autonomous mode id */
+  private int mode = 0;
+
+  /* Initialize and define autonomous modes list */
+  String[] autoList = { "" };
+
+  /* Define gyroscope class */
+  public static final ADIS16448_IMU imu = new ADIS16448_IMU();
+
   @Override
   public void robotInit() {
-    m_oi = new OI();
-    m_chooser.setDefaultOption("Default Auto", new ExampleCommand());
-    // chooser.addOption("My Auto", new MyAutoCommand());
-    SmartDashboard.putData("Auto mode", m_chooser);
+    /* Initialize RobotMap */
+    RobotMap.init();
+
+    /* Define OI and Subsystems */
+    oi = new OI();
+
+    /* Push autonomous list into NetworkTables */
+    NetworkTable table = NetworkTable.getTable("SmartDashboard");
+    table.putStringArray("AutoList", autoList);
+
+    /* Select default autonomous mode */
+    table.putNumber("autoSelected", 0);
   }
 
-  /**
-   * This function is called every robot packet, no matter the mode. Use
-   * this for items like diagnostics that you want ran during disabled,
-   * autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before
-   * LiveWindow and SmartDashboard integrated updating.
-   */
   @Override
   public void robotPeriodic() {
+    /* Send gyroscope data to NetworkTables */
+    table.putNumber("Gyro-X", imu.getAngleX());
+    table.putNumber("Gyro-Y", imu.getAngleY());
+    table.putNumber("Gyro-Z", imu.getAngleZ());
+
+    /* Send battery percentage to NetworkTables */
+    table.putNumber("battery", DriverStation.getInstance().getBatteryVoltage());
   }
 
-  /**
-   * This function is called once each time the robot enters Disabled mode.
-   * You can use it to reset any subsystem information you want to clear when
-   * the robot is disabled.
-   */
   @Override
   public void disabledInit() {
   }
@@ -67,37 +64,21 @@ public class Robot extends TimedRobot {
     Scheduler.getInstance().run();
   }
 
-  /**
-   * This autonomous (along with the chooser code above) shows how to select
-   * between different autonomous modes using the dashboard. The sendable
-   * chooser code works with the Java SmartDashboard. If you prefer the
-   * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-   * getString code to get the auto name from the text box below the Gyro
-   *
-   * <p>You can add additional auto modes by adding additional commands to the
-   * chooser code above (like the commented example) or additional comparisons
-   * to the switch structure below with additional strings & commands.
-   */
   @Override
   public void autonomousInit() {
-    m_autonomousCommand = m_chooser.getSelected();
 
-    /*
-     * String autoSelected = SmartDashboard.getString("Auto Selector",
-     * "Default"); switch(autoSelected) { case "My Auto": autonomousCommand
-     * = new MyAutoCommand(); break; case "Default Auto": default:
-     * autonomousCommand = new ExampleCommand(); break; }
-     */
+    /* Set mode variable to the chosen autonomous mode id */
+    mode = (int) table.getNumber("autoSelected", 0);
 
-    // schedule the autonomous command (example)
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.start();
+    /* Set autonomousCommand to the right command according to the mode variable */
+    // ACTUALLY SET THE MODES WHEN WE HAVE THEM :)
+
+    /* Start the autonomous command if it has not been started already */
+    if (autonomousCommand != null) {
+      autonomousCommand.start();
     }
   }
 
-  /**
-   * This function is called periodically during autonomous.
-   */
   @Override
   public void autonomousPeriodic() {
     Scheduler.getInstance().run();
@@ -105,26 +86,18 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopInit() {
-    // This makes sure that the autonomous stops running when
-    // teleop starts running. If you want the autonomous to
-    // continue until interrupted by another command, remove
-    // this line or comment it out.
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
+
+    /* Stop the autonomous if it is still running */
+    if (autonomousCommand != null) {
+      autonomousCommand.cancel();
     }
   }
 
-  /**
-   * This function is called periodically during operator control.
-   */
   @Override
   public void teleopPeriodic() {
     Scheduler.getInstance().run();
   }
 
-  /**
-   * This function is called periodically during test mode.
-   */
   @Override
   public void testPeriodic() {
   }
